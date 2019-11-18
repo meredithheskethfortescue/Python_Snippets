fixed rotation 1d wavelet to 2d

plot.py
@@ -1,19 +1,21 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_quiver(img: arraylike):
+def plot_quiver(img):
     __, height, width = np.shape(img)
     x = list(range(width))
     y = list(reversed(range(height)))
 
     fig, ax = plt.subplots()
-    ax.quiver(x, y, img[1], img[0])
+    ax.quiver(x, y, img[0], img[1])
     plt.show()
 
 
-def plot_wireframe(two_dim_array: arraylike, show=True):
+def plot_wireframe(two_dim_array, show=True):
     """Setup a 2d array as 3d-plot and show window.
     :param two_dim_array: 2d array of data to be plotted
     :param show: If True open up window directly
@@ -31,15 +33,25 @@ def plot_wireframe(two_dim_array: arraylike, show=True):
     return surf
 
 
+def cone(scope, samples):
+    t_squared = np.linspace(-scope, scope, samples) ** 2
+    line_2d = np.empty((samples, samples))
+    line_2d[:] = t_squared
+    return np.sqrt(line_2d + np.transpose(line_2d))
+
+
+def morlet(t, f):
+    s = 7 / (2 * np.pi * f)
+    return np.cos(2 * np.pi * f * t) * np.exp(- t ** 2 / (2 * s ** 2))
+
+
 if __name__ == '__main__':
-    def morlet(t, f):
-        s = 7 / (2 * np.pi * f)
-        return np.cos(2 * np.pi * f * t) * np.exp(- t ** 2 / (2 * s ** 2))
+    scope = np.pi
+    samples = 31
 
-    t = np.linspace(-2, 2, 201)
-
-    wavelet_1d = morlet(t, 2)
-    wavelet_2d = np.outer(wavelet_1d, wavelet_1d)
+    wavelet_1d = np.cos(np.linspace(-scope, scope, samples))
+    cone = cone(scope, samples)
+    wavelet_2d = np.cos(cone)
 
     plt.plot(wavelet_1d)
     plt.show()