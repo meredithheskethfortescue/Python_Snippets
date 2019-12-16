f2e33d12f0ec093621e3e888f1d3c279f9762bc4
Meredith Hesketh-Fortescue <30291320+meredithheskethfortescue@users.noreply.github.com>
2019-12-16 11:33:20 +0100

    fixed copy paste error in ls -la

--- list_files_in_directory.py
+++ list_files_in_directory.py
@@ -5,7 +5,7 @@
 dir_data = './data/'
 
 # list all files
-filenames_all = [os.path.splitext(f)[0] for f in os.listdir(dir_data) if f.endswith('.png')]
+filenames_all = [f for f in os.listdir(dir_data)]
 
 # list all *.png files in the given directory and split suffix from filename
 filenames_glob_nosuffix = [os.path.splitext(f)[0] for f in os.listdir(dir_data) if f.endswith('.png')]
