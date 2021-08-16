5c4546c35618ff232b7fe13052cbaace8954248b
Meredith Hesketh-Fortescue <30291320+meredithheskethfortescue@users.noreply.github.com>
2021-08-16 10:24:21 +0200

    added whole setup process

--- create_venv.sh
+++ create_venv.sh
@@ -1,2 +1,17 @@
+# save list of installed packages to file -> should be edited for necessary packages only
+pip freeze > requirements.txt
+
 # create virtual python environment from existing installation
 python3.10.0 -m venv venv
+
+# activate (posix)
+source ./venv/bin/activate
+
+# activate (windows)
+# venv\Scripts\activate
+
+# install requirements
+pip install -r requirements.txt
+
+# deactivate (posix/windows)
+deactivate
