2477a57d20948463a9dc2cf7c20daaadee4b9b30
Meredith Hesketh-Fortescue <30291320+meredithheskethfortescue@users.noreply.github.com>
2023-01-25 13:12:02 +0100

    added optional `symbol` parameter

--- pretty_print_decorator.py
+++ pretty_print_decorator.py
@@ -4,33 +4,41 @@ import functools
 import io
 
 
-def box(func):
+def box(_func=None, symbol: str = "="):
     """Decorator to draw a bar above and below the standard output of a function"""
+    assert len(symbol) > 0, KeyError("`symbol` value must contain a character")
+    assert len(symbol) == 1, NotImplementedError("Multiple symbols are not supportet yet!")
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        # catch stdout from wrapped function
-        stdout = io.StringIO()
-        with redirect_stdout(stdout):
-            func(*args, **kwargs)
-        msg = stdout.getvalue()
+    def decorator_box(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # catch stdout from wrapped function
+            stdout = io.StringIO()
+            with redirect_stdout(stdout):
+                func(*args, **kwargs)
+            msg = stdout.getvalue()
 
-        # stay quiet if stdout is empty
-        if not msg:
-            return
+            # stay quiet if stdout is empty
+            if not msg:
+                return
 
-        # the longest line in output determines the length of the bars
-        max_len = max([len(line) for line in msg.splitlines()])
+            # the longest line in output determines the length of the bars
+            max_len = max([len(line) for line in msg.splitlines()])
 
-        # print bar above stdout
-        print(max_len * "=")
-        # re-redirect message to stdout but strip trailing whitespace in order to
-        # place the trailing bar tight below the message
-        print(msg.rstrip())
-        # print bar below stdout
-        print(max_len * "=" + "\n")
+            # print bar above stdout
+            print(max_len * symbol)
+            # re-redirect message to stdout but strip trailing whitespace in order to
+            # place the trailing bar tight below the message
+            print(msg.rstrip())
+            # print bar below stdout
+            print(max_len * symbol + "\n")
 
-    return wrapper
+        return wrapper
+
+    if _func is None:
+        return decorator_box
+    else:
+        return decorator_box(_func)
 
 
 if __name__ == '__main__':
@@ -41,6 +49,11 @@ if __name__ == '__main__':
         print("Lorem ipsum, dolor sit.")
         print("Amet consectetuer, adipiscing")
         print("elitr ut purus invidunt.")
+
+
+    @box(symbol="+")
+    def bark():
+        print("Bleh!")
 
 
     @box
@@ -54,6 +67,9 @@ if __name__ == '__main__':
     # a very long name will cause the bar length to be extended in order to fit the content
     verbose_function("XavlegbmaofffassssitimiwoamndutroabcwapwaelippohfffX")
     verbose_function("Phoebe")
+
+    # this one has a different symbol
+    bark()
 
     # a function call that does not print to stdout stays silent despite being decorated
     silent_function()
