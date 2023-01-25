608a6bbe57b04f15c50d1e11f7de06d7b1a5ebcb
Meredith Hesketh-Fortescue <30291320+meredithheskethfortescue@users.noreply.github.com>
2023-01-25 12:14:07 +0100

    added trailing newline

--- pretty_print_decorator.py
+++ pretty_print_decorator.py
@@ -1,11 +1,13 @@
 """Highlight print messages"""
-import io
 from contextlib import redirect_stdout
+import functools
+import io
 
 
-def emphasize_stdout(func):
-    """Decorator to draw a line above and below the standard output of a function"""
+def box(func):
+    """Decorator to draw a bar above and below the standard output of a function"""
 
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         # catch stdout from wrapped function
         stdout = io.StringIO()
@@ -17,27 +19,41 @@ def emphasize_stdout(func):
         if not msg:
             return
 
-        # the longest line in output determines the length of the lines
+        # the longest line in output determines the length of the bars
         max_len = max([len(line) for line in msg.splitlines()])
 
-        # print line above stdout
+        # print bar above stdout
         print(max_len * "=")
         # re-redirect message to stdout but strip trailing whitespace in order to
-        # place the trailing line tight below the message
+        # place the trailing bar tight below the message
         print(msg.rstrip())
-        # print line below stdout
-        print(max_len * "=")
+        # print bar below stdout
+        print(max_len * "=" + "\n")
 
     return wrapper
 
 
-@emphasize_stdout
-def verbose_function(world: str = "world"):
-    print(f"Hello, {world}!")
-    print()
-    print("Lorem ipsum, dolor sit.")
-    print("Amet consectetuer, adipiscing elitr ut purus invidunt.")
-
-
 if __name__ == '__main__':
-    verbose_function("Alpha Centauri")
+    @box
+    def verbose_function(world: str = "world"):
+        print(f"Hello, {world}!")
+        print()
+        print("Lorem ipsum, dolor sit.")
+        print("Amet consectetuer, adipiscing")
+        print("elitr ut purus invidunt.")
+
+
+    @box
+    def silent_function():
+        pass
+
+
+    # bar adapts to the max line length - in these cases the body.
+    verbose_function("Eros")
+    verbose_function("Io")
+    # a very long name will cause the bar length to be extended in order to fit the content
+    verbose_function("XavlegbmaofffassssitimiwoamndutroabcwapwaelippohfffX")
+    verbose_function("Phoebe")
+
+    # a function call that does not print to stdout stays silent despite being decorated
+    silent_function()
